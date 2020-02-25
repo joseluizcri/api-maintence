6a1e2c6ef232fc2b1ce3c63a2bd3e65da8d8ccd1
José Luiz <junior.joseluizrosa@gmail.com>
2020-02-25 13:38:06 -0300

    Commit inicial

---
+++ src/main/java/br/com/joseluizrosajunior/maintence/MaintenceApplication.java
@@ -0,0 +1,27 @@
+package br.com.joseluizrosajunior.maintence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+@EnableResourceServer
+public class MaintenceApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenceApplication.class, args);
+	}
+
+}
